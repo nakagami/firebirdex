ca86b2df075a188c224106ca2b2e403246928fa3
Hajime Nakagami <nakagami@gmail.com>
2019-03-21 12:48:24 +0900

    modify error message

--- lib/firebirdex/error.ex
+++ lib/firebirdex/error.ex
@@ -1,13 +1,18 @@
 defmodule Firebirdex.Error do
-  defexception [
-    :number,
-    :message,
-    :statement
-  ]
+  defexception [:number, :reason, :statement]
 
   @type t :: %__MODULE__{
-          number: integer(),
-          message: String.t(),
-          statement: iodata() | nil
-        }
+    number: integer(),
+    reason: String.t(),
+    statement: iodata() | nil
+  }
+
+  def message(e) do
+    if e.statement == nil do
+      "#{e.number}:#{e.reason}"
+    else
+      "#{e.number}:#{e.reason}\t#{IO.iodata_to_binary(e.statement)}"
+    end
+  end
+
 end
