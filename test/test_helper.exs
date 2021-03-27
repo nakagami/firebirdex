167dd4cb49e78bf2fadf52027b95f8993c9d034d
Hajime Nakagami <nakagami@gmail.com>
2021-03-28 08:06:39 +0900

    get test user and password from environment

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -6,8 +6,8 @@ defmodule TestHelpers do
       :io_lib.format("/tmp/~p.fdb", [:erlang.system_time()]))
     [
       hostname: "localhost",
-      username: "sysdba",
-      password: "masterkey",
+      username: System.get_env("ISC_USER", "sysdba"),
+      password: System.get_env("ISC_PASSWORD", "masterkey"),
       database: database,
       createdb: true,
       show_sensitive_data_on_connection_error: true
