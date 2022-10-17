b7d5d1b12d64196c226bd4f8e127fb08e87bfd15
Hajime Nakagami <nakagami@gmail.com>
2022-10-17 22:29:08 +0900

    upgrade dependent efirebirdsql

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Firebirdex.Mixfile do
       {:db_connection, "~> 2.4"},
       {:decimal, "~> 2.0"},
       {:timex, "~> 3.7"},
-      {:efirebirdsql, "~> 0.9"},
+      {:efirebirdsql, "~> 0.9.1"},
       {:codepagex, "~> 0.1.6"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
