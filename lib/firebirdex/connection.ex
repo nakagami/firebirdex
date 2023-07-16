fa60ce3dffafe080f5e72897722bed306e9e4795
Hajime Nakagami <nakagami@gmail.com>
2023-07-16 11:20:50 +0900

    fix unused local variable

--- lib/firebirdex/connection.ex
+++ lib/firebirdex/connection.ex
@@ -97,7 +97,7 @@ defmodule Firebirdex.Connection do
         {:ok, rows, stmt} = :efirebirdsql_protocol.fetchall(state.conn, stmt)
         columns = Enum.map(:efirebirdsql_protocol.columns(stmt), &(column_name(&1)))
         {:ok, num_rows} = :efirebirdsql_protocol.rowcount(state.conn, stmt)
-        {:ok, conn} = :efirebirdsql_protocol.free_statement(state.conn, stmt, :drop)
+        {:ok, _conn} = :efirebirdsql_protocol.free_statement(state.conn, stmt, :drop)
         {:ok, %Query{query | stmt: stmt}, %Result{columns: columns, num_rows: num_rows, rows: rows}, %__MODULE__{state | conn: state.conn}}
       {:error, number, reason} ->
         {:error, %Error{number: number, reason: reason, statement: query.statement}, %__MODULE__{state | conn: state.conn}}
